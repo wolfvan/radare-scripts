5197e9fd49bc1523e30bf7b7c5c8879bfc5ba91c
wolfvan <joanbtl@hotmail.com>
2017-04-24 23:52:54 +0200

    XOR hardcoded passwords

---
+++ mirai_decryptor.py
@@ -0,0 +1,40 @@
+#!/usr/python
+
+import r2pipe
+import sys
+
+global MIRAI_FILE
+MIRAI_FILE = ""
+
+
+def load_arg():
+	MIRAI_FILE = sys.argv[1]
+	print MIRAI_FILE
+
+def open_bin():
+	f = r2pipe.open(MIRAI_FILE)
+	f.cmd("aaa")
+	#f.cmd("doo")
+	f.cmd("oo+")
+	return f
+
+
+def main(f):
+	a = f.cmd("iS~rodata")
+	vaddr = a.split(" ")[1]
+	nvaddr = vaddr.split("=")[1]
+	f.cmd("s "+nvaddr)
+
+	f.cmd("s +252")
+	f.cmd("b 400")
+	#offset de 250
+	f.cmd("wox 22 ") #22 es la clave
+	c = f.cmd("px 300")
+	print (f.cmd("ps 400"))
+
+
+if __name__ == "__main__":
+	load_arg()
+	f = open_bin()
+	#extract_add_auth_entry(f)
+	main(f)
